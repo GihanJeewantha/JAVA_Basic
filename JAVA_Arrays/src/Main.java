8b3b1289bd771204f49939d9321a323a6e4a226f
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2023-06-27 00:20:50 +0530

    JAVA Arrays

---
+++ JAVA_Arrays/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main (String [] args){
+        int [] numbers = {2,4,6,8,};
+        String [] names = {"kusal","pathum","sadeera"};
+
+        System.out.println(names[2]);
+
+        names[1] = "Dimuth";
+
+        System.out.println(names[1]);
+    }
+}
---
+++ JAVA_Arrays/src/Second.java
@@ -0,0 +1,13 @@
+public class Second {
+    public static void main(String [] args){
+        String[] names = new String[5];
+
+        names[0]= "pathum";
+        names[1]= "de silva";
+        names[4]= "charith";
+
+        System.out.println(names[4]);
+    }
+
+
+}
