bd8d9ff5ef9be8f547e4b15bc34c587ef2921f95
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2023-06-28 00:08:34 +0530

    Add files via upload

---
+++ String/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String [] args){
+        String s1 = "JavaCode";
+        String s2 = "JavaCode";
+
+        if(s1 == s2){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+
+
+    }
+}
---
+++ String/src/Second.java
@@ -0,0 +1,13 @@
+public class Second {
+    public static void main(String [] args){
+        String s3 = new String("Java");
+        String s4 = new String("Java");
+
+        if(s3 == s4){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+
+    }
+}
