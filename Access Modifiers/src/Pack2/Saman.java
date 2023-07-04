114595da3ac76c973b0a278f8497da8d90a0376c
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2023-07-04 11:17:50 +0530

    Add files via upload

---
+++ Access Modifiers/src/Pack2/Saman.java
@@ -0,0 +1,5 @@
+package Pack2;
+
+ public class Saman {
+ // when we use public it can access anywhere but if we us default as the Access modifire then it can use only the same package class only
+}
